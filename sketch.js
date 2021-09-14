116982303d688a2c11ee0e226360a7910998219f
SniffRat007 <90678878+SniffRat007@users.noreply.github.com>
2021-09-14 17:13:53 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var weight=[35,38,40,45]
+function average(){
+sum=weight[0]+weight[1]+weight[2]+weight[3]
+avg=sum/weight.length
+console.log(avg)
+}
+
+function setup() 
+{
+  createCanvas(400,400);
+  average()
+}
+
+function draw() 
+{
+background(51);
+
+}
+
